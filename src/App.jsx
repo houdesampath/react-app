2a7bbb0b4695aaf8e6f3f3447b75bb15d0652662
houdesampath <sampath.kumar@wadhwaniai.org>
2024-02-10 15:03:24 +0530

    addded hasb router

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { HashRouter, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import Proposal from './pages/Proposal';
 import './static/styles/app.css';
 
 function App() {
     return (
-        <BrowserRouter>
+        <HashRouter>
             <Routes>
                 <Route path="/:id" element={<Proposal />} />
                 <Route path="/" element={<Home />} />
             </Routes>
-        </BrowserRouter>
+        </HashRouter>
     );
 }
 
