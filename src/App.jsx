c2beaf93f3e761e12c14d5b317085c6bd49500b2
houdesampath <sampath.kumar@wadhwaniai.org>
2024-02-10 15:24:09 +0530

    browser router

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,17 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { HashRouter, Route, Routes } from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import Proposal from './pages/Proposal';
 import './static/styles/app.css';
 
 function App() {
     return (
-        <HashRouter>
+        <BrowserRouter basename='/react-app'>
             <Routes>
                 <Route path="/:id" element={<Proposal />} />
                 <Route path="/" element={<Home />} />
             </Routes>
-        </HashRouter>
+        </BrowserRouter>
     );
 }
 
